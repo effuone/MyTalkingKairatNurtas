6526936b2104eac1a7f2049118b93ee1d9152c61
effuone <albkfil@gmail.com>
2022-07-13 12:24:55 +0600

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Content from "../components/common/content/Content"
 import About from "../pages/About"
-import MusicPage from "../pages/MusicPage"
-import Content from '../components/common/content/Content'
 export const publicRoutes = [
     {path: '/about', element: <About/>, exact: true},
-    {path: '/music', element: <MusicPage/>, exact: true},
     {path: '/play', element: <Content />, exact: true}
 ]
