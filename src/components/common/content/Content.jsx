8a9f6f1efbd6c875e15f034ee2d6693a82c2b474
effuone <albkfil@gmail.com>
2022-07-13 07:16:14 +0600

    url fix

--- src/components/common/content/Content.jsx
+++ src/components/common/content/Content.jsx
@@ -4,16 +4,23 @@ import { Canvas } from "react-three-fiber";
 import './Content.module.css'
 import Kaireke from "./Kaireke";
 import SongService from "../../../API/SongService";
+import ReactAudioPlayer from "react-audio-player";
 const Content = () => {
     const [action, setAction] = useState("Idle");
     const [songs, setSongs] = useState([])
-    const [currentSong, setCurrentSong] = useState(songs[0])
-    const fetchSongs = () => {
-        setSongs(await SongService.getSongs().data)
+    const [currentSong, setCurrentSong] = useState(new Audio(songs[0]))
+    const fetchSongs = async () => {
+        const response = await SongService.getSongs()
+        setSongs(response.data)
     }
-    useEffect()
+    useEffect(()=>{
+        fetchSongs()
+    }, [songs])
     const changeSong = () => {
-        
+        const curr = (songs[Math.floor(Math.random() * songs.length)]);
+        setCurrentSong(new Audio(curr.url))
+        console.log(currentSong)
+        currentSong.play()
     }
     return (
         <>
@@ -23,7 +30,7 @@ const Content = () => {
                 <button onClick={() => setAction("snakedance")}>Snakedance</button>
                 <button onClick={() => setAction("twerkingdance")}>twerkingdance</button>
                 <button onClick={() => setAction("zombiehit")}>zombiehit</button>
-                <button onClick={changeSong}>zombiehit</button>
+                <button onClick={changeSong}>Change Track</button>
             </div>
             <Canvas camera={{ position: [0, 0.2, 2] }}>
                 <pointLight intensity={2} position={[-1, 15, 10]} color="green" />
