968915b9f60d7e0dd7219a9f9a5dd9340a73c8f4
Alibek Seitov <65224356+effuone@users.noreply.github.com>
2022-07-13 14:22:42 +0600

    Update Header.jsx

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return(
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">My DAncing Kaireke</Navbar.Brand>
+          <Navbar.Brand href="/">My Dancing Kaireke</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/play">Play</Nav.Link>
